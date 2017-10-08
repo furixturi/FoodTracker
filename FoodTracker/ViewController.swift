8964662cc95e91b26020a65f7baa49c3352dae9e
furixturi <alabebop@gmail.com>
2017-10-08 12:41:34 +0900

    add text field delegate methods to use the text field to change the label text

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,25 +8,35 @@
 
 import UIKit
 
-
-
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Handle the text field's user input through delegate callbacks.
+        nameTextField.delegate = self
+    }
+    
     //MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
     }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+    //MARK: UITextFieldDelegate
+    // Called when "Return(Done)" is tapped.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    // Called after textField resigns the first-responder status
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
 //    override func didReceiveMemoryWarning() {
 //        super.didReceiveMemoryWarning()
 //        // Dispose of any resources that can be recreated.
