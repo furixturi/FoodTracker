6683415b10c7fafd4c53ed40dbfe44ade9d91849
furixturi <alabebop@gmail.com>
2017-10-09 01:08:58 +0900

    add custom control RatingControl

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Xiaoli Shen on 2017/10/09.
+//  Copyright © 2017 furixturi. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
