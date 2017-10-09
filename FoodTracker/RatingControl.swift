66db40428e256c8c4861edd255a4eecf1d6d48c2
furixturi <alabebop@gmail.com>
2017-10-09 23:09:57 +0900

    add interface builder attribute inspection support to custom control

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -12,6 +12,17 @@ import UIKit
     //MARK: Properties
     private var ratingButtons = [UIButton]()
     var rating = 0
+    // property observers
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
     
     //MARK: Initialization
     //For programmatically instantiate the view, called by Interface Builder at app development
@@ -33,15 +44,25 @@ import UIKit
     
     //MARK: Private Methods
     private func setupButtons() {
+        // Clear any existing buttons
+        for button in ratingButtons {
+            // Tell the stack view no longer to calculate the button's size and position
+            removeArrangedSubview(button)
+            // Remove the button from the stack view entirely
+            button.removeFromSuperview()
+        }
+        // Empty the ratingButtons array
+        ratingButtons.removeAll()
+        
         // Create the buttons
-        for _ in 0..<5 {
+        for _ in 0..<starCount {
             let button = UIButton()
             button.backgroundColor = UIColor.red
             
             // Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false // Disables the button's auto-generated constraints
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             
             // Setup the button action
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
@@ -49,7 +70,7 @@ import UIKit
             // Add the button to the stack
             addArrangedSubview(button)
             
-            // Add the new button to the rating button array
+            // Add the new button to the ratingButtons array
             ratingButtons.append(button)
         }
     }
