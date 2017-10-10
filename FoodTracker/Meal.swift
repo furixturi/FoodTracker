3168316887cd726b8840a0e201abc062a7c61a31
furixturi <alabebop@gmail.com>
2017-10-11 00:45:24 +0900

    unit test Meal class

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -12,18 +12,19 @@ class Meal {
     
     //MARK: Properties
     var name: String
-    var photo: UIImage
+    var photo: UIImage?
     var rating: Int
     
     //MARK: Initialization
     init?(name: String, photo: UIImage?, rating: Int) {
         // Initialization should fail if there is no name or if the rating is negative
+        
         if name.isEmpty || rating < 0 {
             return nil
         }
         
         self.name = name
-        self.photo = photo!
+        self.photo = photo
         self.rating = rating
         
     }
