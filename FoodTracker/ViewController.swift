e606f6b8515d405d4c979c9e16b452b9531fdb4c
furixturi <alabebop@gmail.com>
2017-10-10 21:09:47 +0900

    clean up setDefaultText button and center the layout

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -23,9 +23,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
     
     //MARK: Actions
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
         
         // Hide the keyboard
