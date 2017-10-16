bc2889b0ebf6911c86b08f5d8a9f9a28bf477826
furixturi <alabebop@gmail.com>
2017-10-16 18:57:18 +0900

    Add class for custom UITableViewCell, MealTableViewCell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Xiaoli Shen on 2017/10/16.
+//  Copyright © 2017 furixturi. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
