a8fc503d67ca1ba7f9789098ce491fbe5561b1d6
furixturi <alabebop@gmail.com>
2017-10-17 23:05:47 +0900

    dismiss MealDetailView conditionally

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -77,7 +77,21 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     }
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways
+        
+        // If the view was presented by a UINavigationController, it is presented by the user clicking add button
+        let isPresentingInAddMealModel = presentingViewController is UINavigationController
+        
+        if isPresentingInAddMealModel {
+            dismiss(animated: true, completion: nil)
+        }
+        //
+        else if let owningNavigationController = navigationController {
+            owningNavigationController.popViewController(animated: true)
+        }
+        else {
+            fatalError("The MealViewController is not inside a navigation controller")
+        }
     }
     
     //MARK: UITextField Delegate
