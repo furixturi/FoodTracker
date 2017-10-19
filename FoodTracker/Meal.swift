6164e11b0827cd7a2a7289aaf17678fdcf96d61a
furixturi <alabebop@gmail.com>
2017-10-19 18:00:56 +0900

    use NSCoding to enable saving Meal data

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import os.log
 
-class Meal {
+class Meal: NSObject, NSCoding {
     
     //MARK: Properties
     var name: String
     var photo: UIImage?
     var rating: Int
     
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
+    
+    // MARK: Archiving Paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+
     //MARK: Initialization
     init?(name: String, photo: UIImage?, rating: Int) {
         // The name must not be empty
@@ -30,5 +41,30 @@ class Meal {
         self.photo = photo
         self.rating = rating
         
+    }
+    
+    // MARK: NSCoding Methods
+    // Prepares the class's information to be archived
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    // Unarchives the data when the class is created
+    // convenience modifier means that this is a secondary initializer, and that it must call a designated initializer from the same class
+    // ? means it is failable, i.e., it might return nil
+    required convenience init?(coder aDecoder: NSCoder) {
+        // The name is required. Return nil if we cannot decode a name string
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String
+            else {
+                os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
+                return nil
+        }
+        
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        let rating = aDecoder.decodeObject(forKey: PropertyKey.rating) as! Int
+        
+        // Must call designated initializer
+        self.init(name: name, photo: photo, rating: rating)
     }
 }
